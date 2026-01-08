4e2768dafd2fcbf3c1c93b0f21466cefbbe5f837
NovaNoctua <m01@naudet.ch>
2026-01-08 15:56:45 +0100

    feat(games): added games controller

--- app/controllers/games_controller.ts
+++ app/controllers/games_controller.ts
@@ -1,4 +1,14 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Game from '#models/game'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class GamesController {
+  async index({ response }: HttpContext) {
+    const games = await Game.query().orderBy('generation', 'asc').orderBy('id', 'asc')
+    return response.ok(games)
+  }
+
+  async show({ params, response }: HttpContext) {
+    const game = await Game.findOrFail(params.id)
+    return response.ok(game)
+  }
 }
