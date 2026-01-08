830fc59f0d5978b607e4064560cc3f691a456db2
NovaNoctua <m01@naudet.ch>
2026-01-08 15:48:43 +0100

    feat(pokemon): pokemons can all be read  and can be read one by one

--- app/controllers/pokemon_controller.ts
+++ app/controllers/pokemon_controller.ts
@@ -1,4 +1,23 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Pokemon from '#models/pokemon'
 
 export default class PokemonController {
+  async index({ request, response }: HttpContext) {
+    const page = request.input('page', 1)
+
+    const limit = 20
+
+    const pokemon = await Pokemon.query().orderBy('id', 'asc').paginate(page, limit)
+
+    return response.ok(pokemon)
+  }
+
+  async show({ params, response }: HttpContext) {
+    try {
+      const pokemon = await Pokemon.findOrFail(params.id)
+      return response.ok(pokemon)
+    } catch (error) {
+      return response.notFound({ message: 'This pokemon number does not exist' })
+    }
+  }
 }
