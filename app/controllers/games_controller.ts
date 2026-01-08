6e5cfec1011e8f3727ce9726726214d51f93661c
NovaNoctua <m01@naudet.ch>
2026-01-08 16:20:40 +0100

    feat(methods in game): put all methods in the game controller show and index

--- app/controllers/games_controller.ts
+++ app/controllers/games_controller.ts
@@ -3,12 +3,15 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class GamesController {
   async index({ response }: HttpContext) {
-    const games = await Game.query().orderBy('generation', 'asc').orderBy('id', 'asc')
+    const games = await Game.query()
+      .orderBy('generation', 'asc')
+      .orderBy('id', 'asc')
+      .preload('methods')
     return response.ok(games)
   }
 
   async show({ params, response }: HttpContext) {
-    const game = await Game.findOrFail(params.id)
+    const game = await Game.query().where('id', params.id).preload('methods').firstOrFail()
     return response.ok(game)
   }
 }
--- app/models/game.ts
+++ app/models/game.ts
@@ -1,9 +1,10 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
+import { BaseModel, column, hasMany, manyToMany } from '@adonisjs/lucid/orm'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
 import Entry from './entry.js'
 import Hunt from './hunt.js'
 import MethodsInGame from './methods_in_game.js'
+import Method from './method.js'
 
 export default class Game extends BaseModel {
   // Attributes
@@ -37,4 +38,11 @@ export default class Game extends BaseModel {
 
   @hasMany(() => MethodsInGame)
   declare methodsInGame: HasMany<typeof MethodsInGame>
+
+  @manyToMany(() => Method, {
+    pivotTable: 'methods_in_games', // Your middle table name
+    pivotForeignKey: 'game_id', // Column in middle table pointing to Game
+    pivotRelatedForeignKey: 'method_id', // Column in middle table pointing to Method
+  })
+  declare methods: ManyToMany<typeof Method>
 }
