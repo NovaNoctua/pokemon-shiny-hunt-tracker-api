a058c154c531eed83d7031d94349cc85bfce574c
NovaNoctua <m01@naudet.ch>
2026-01-08 16:23:16 +0100

    feat(methods): index and show of method controller

--- app/controllers/methods_controller.ts
+++ app/controllers/methods_controller.ts
@@ -1,5 +1,14 @@
+import Method from '#models/method'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MethodsController {
-  async index({ response }: HttpContext) {}
+  async index({ response }: HttpContext) {
+    const methods = await Method.query().orderBy('name', 'asc')
+    return response.ok(methods)
+  }
+
+  async show({ params, response }: HttpContext) {
+    const method = await Method.findOrFail(params.id)
+    return response.ok(method)
+  }
 }
